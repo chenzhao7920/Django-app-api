9a2544d54d0b836381fe27fa8084e0add359cf7b
chenzhao7920 <chenzhao7920@gmail.com>
2025-07-28 20:29:15 -0400

    feat: implement user model, test and migration

--- app/core/models.py
+++ app/core/models.py
@@ -1,25 +1,31 @@
 # """
 # Database models
 # """
-# from django.db import models
-# from django.contrib.auth.models import (
-#     AbstractBaseUser,
-#     BaseUserManager,
-#     PermissionsMixin
-# )
-# class UserManager(BaseUserManager):
-#     """Manager for users"""
-#     def create_user(self, email, password = None, **extra_field):
-#         """CRUD"""
-#         user = self.model(email = email, **extra_field)
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
-# class User(AbstractBaseUser, PermissionsMixin):
-#     """User in the system."""
-#     email = models.EmailField(max_length=255, unique=True)
-#     name = models.CharField(max_length=255)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+class UserManager(BaseUserManager):
+    """Manager for users"""
+    def create_user(self, email, password = None, **extra_fields):
+        """CRUD"""
+        email = self.normalize_email(email)
+        user = self.model(email = email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-#     USERNAME_FIELD = 'email'
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    '''Manager for users'''
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
